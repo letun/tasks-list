a078ceaa13c1a97013fbe991381274e334decc94
letun1984@gmail.com <sergei@Sergeis-MacBook-Pro-2.local>
2025-02-27 16:15:53 +0300

    Реализовать возможность добавления новой таски

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,22 +1,42 @@
 import React from "react";
-import TaskItem from "./TaskItem"
+import { useState } from "react";
+
+import TaskItem from "./TaskItem";
 
 const getTasks = () =>
-  Array.from({ length: 10 }, (_, i) => ({
-    id: i,
-    text: `Task ${i + 1}`,
-    completed: false,
-  }));
+    Array.from({ length: 10 }, (_, i) => ({
+        id: i,
+        text: `Task ${i + 1}`,
+        completed: false,
+    }));
+
+function getMaxTaskId(tasks = []) {
+    return getTasks().reduce( (acc, v) => Math.max(acc, v.id), -1 ) + 1;
+}
 
 export default function Tasks() {
-    const tasks = getTasks();
+    const [title, setTitle] = useState('');
+    const [tasks, setTasks] = useState(getTasks());
+    const [lastTaskId, setlastTaskId] = useState(getMaxTaskId(tasks));
+
+    function handleAddTask() {
+        if (title) {
+            setlastTaskId((id) => ++id);
+            setTasks( (tasks) => [...tasks, {
+                    id: lastTaskId,
+                    text: title,
+                    completed: false,
+                }]
+            );
+            setTitle('');
+        }
+    }
 
   return (
     <div>
       <h1>Task List</h1>
-
-      <input type="text" />
-      <button>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button onClick={handleAddTask}>
         Add Task
       </button>
       <ul>
